2f1fe1b177ae8a410c86e55049df96f271243339
codeinsack <codeinsack@gmail.com>
2021-01-08 18:38:47 +0300

    make initial settings

---
+++ src/components/HelloWorld/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">HELLO WORLD</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const { logout, user } = useApp();
+    return {
+      logout,
+      user,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
